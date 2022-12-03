d87182bf0dd004461d61a0e79a86afd6dc9cfb6f
vasquez-a <43757440+vasquez-a@users.noreply.github.com>
2022-12-03 16:42:36 -0500

    getting the autoscrolling to work

--- main.js
+++ main.js
@@ -3,19 +3,18 @@ let counter = 0;
 let max = 0;
 
 $(document).ready(function(){
-  $.getJSON("poemtext.json", function(result){
-    fulltext = result.texts;
-    max = result.texts.length;
-    //console.log(fulltext + "\n" + max);
+  //document.getElementById('mainToastContainer').focus();
+  const toastElList = document.querySelectorAll('.toast');
+  const toastList = [...toastElList].map(toastEl => new bootstrap.Toast(toastEl));
+  $("#continueButton").click(function(){
+    toastList[counter].show();
+    toastElList[counter].scrollIntoView();
+    //toastElList[counter].focus();
+    counter++;
   });
 });
 
-$("#continueButton").click(function(){
-  if(counter < max){
-      $("#mainToastContainer").append("<div class='toast show' role='alert' aria-live='assertive' aria-atomic='true'> <div class='toast-header'><img src='...' class='rounded me-2' alt='...'><strong class='me-auto'>Bootstrap</strong><small class='text-muted'>just now</small><button type='button' class='btn-close' data-bs-dismiss='toast' aria-label='Close'></button></div><div class='toast-body wsans'>" + fulltext[counter] + "</div></div>");
-      counter++;
-    }
-});
+
 
 /*
 
